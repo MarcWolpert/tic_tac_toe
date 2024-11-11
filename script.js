dbe33612a6be20f9081143ab7d3cf54d53c6016d
Marc Wolpert <mewolpert@gmail.com>
2024-11-10 17:04:44 -0800

    feat: Updated to allow 2 players to play the game

--- script.js
+++ script.js
@@ -12,26 +12,12 @@ Player.prototype.makeMove = function(board, index) {
     return false;
 };
 
-function Computer(symbol) {
-    Player.call(this, symbol);
-}
-
-Computer.prototype = Object.create(Player.prototype);
-Computer.prototype.constructor = Computer;
-
-Computer.prototype.makeRandomMove = function(board) {
-    let moveMade = false;
-    while (!moveMade) {
-        const index = Math.floor(Math.random() * 9);
-        moveMade = this.makeMove(board, index);
-    }
-};
-
 function Board() {
     this.gameTiles = [...document.querySelectorAll(".gameTile")];
     this.gameMoves = Array(3).fill().map(() => Array(3).fill(undefined));
-    this.player = new Player("player");
-    this.computer = new Computer("computer");
+    this.player1 = new Player("X");
+    this.player2 = new Player("O");
+    this.currentPlayer = this.player1; // Start with player 1
     this.moveCount = 0;
     this.gameOver = false;
 
@@ -53,30 +39,31 @@ Board.prototype.resetGame = function() {
     document.getElementById("statusMessage").innerText = "Make your move!";
     this.moveCount = 0;
     this.gameOver = false;
+    this.currentPlayer = this.player1; // Reset to player 1's turn
 };
 
 Board.prototype.handlePlayerMove = function(index) {
     if (this.gameOver) return;
-    if (this.player.makeMove(this, index)) {
+
+    if (this.currentPlayer.makeMove(this, index)) {
         this.moveCount++;
         this.checkGameStatus();
-        if (!this.gameOver) this.computerMove();
+        
+        if (!this.gameOver) {
+            // Switch to the other player
+            this.currentPlayer = this.currentPlayer === this.player1 ? this.player2 : this.player1;
+            document.getElementById("statusMessage").innerText = `${this.currentPlayer === this.player1 ? p1 || "Player 1" : p2 || "Player 2"}'s turn`;
+        }
     }
-};
-
-Board.prototype.computerMove = function() {
-    this.computer.makeRandomMove(this);
-    this.moveCount++;
-    this.checkGameStatus();
 };
 
 Board.prototype.convert1dTo2d = function(index) {
     return [Math.floor(index / 3), index % 3];
 };
 
-Board.prototype.updateTile = function(index, player) {
+Board.prototype.updateTile = function(index, symbol) {
     const tile = this.gameTiles[index];
-    if (player === "player") {
+    if (symbol === "X") {
         tile.style.backgroundColor = "black";
         tile.style.backgroundImage = "url('icons/circle_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg')";
     } else {
@@ -92,7 +79,7 @@ Board.prototype.checkGameStatus = function() {
     const winner = this.checkWinner();
     if (winner) {
         this.gameOver = true;
-        document.getElementById("statusMessage").innerText = winner === "player" ? "Player Wins!" : "Computer Wins!";
+        document.getElementById("statusMessage").innerText = `${winner === "X" ? p1 || "Player 1" : p2 || "Player 2"} Wins!`;
         this.gameTiles.forEach(tile => tile.disabled = true);
     } else if (this.moveCount === 9) {
         this.gameOver = true;
@@ -122,6 +109,32 @@ Board.prototype.checkWinner = function() {
     }
     return null;
 };
+
+// Handle player name inputs
+const player01 = document.getElementById("player01");
+const player02 = document.getElementById("player02");
+const playerName01 = document.getElementById("player01Name");
+const playerName02 = document.getElementById("player02Name");
+let p1 = undefined;
+let p2 = undefined;
+
+player01.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+        e.preventDefault();
+        playerName01.innerText = player01.value;
+        player01.value = "";
+        p1 = playerName01.innerText;
+    }
+});
+
+player02.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+        e.preventDefault();
+        playerName02.innerText = player02.value;
+        player02.value = "";
+        p2 = playerName02.innerText;
+    }
+});
 
 // Initialize the game board
 const gameBoard = new Board();
